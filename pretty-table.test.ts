41c0e31cce8ccff356efca1607254f5464b59454
Simon Wikander <simon.wikander@avanza.se>
2020-05-22 23:21:06 +0200

    Basic table functionality.

---
+++ pretty-table.test.ts
@@ -0,0 +1,160 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import Table from "./pretty-table.ts";
+
+interface F {
+  file: Deno.File;
+  name: string;
+}
+
+async function createFile(): Promise<F> {
+  const tempDir = await Deno.makeTempDir();
+  const name = tempDir + "/table_output.txt";
+  const file = await Deno.open(name, {
+    create: true,
+    write: true,
+  });
+  return { file, name };
+}
+
+async function readFileContent(f: F): Promise<string> {
+  await f.file.close();
+
+  const fileContents = await Deno.readFile(f.name);
+  await Deno.remove(f.name);
+  const decoder = new TextDecoder();
+  return decoder.decode(fileContents);
+}
+
+Deno.test("Basic table", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file });
+
+  await t.write([
+    ["hello world", "test"],
+    ["h", "t"],
+  ]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+    tableText,
+`\
+hello world test
+h           t   \
+`);
+});
+
+Deno.test("Basic table with spacing", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file, spacing: 3 });
+
+  await t.write([
+    ["hello world", "test"],
+    ["h", "t"],
+  ]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+      tableText,
+`\
+hello world   test
+h             t   \
+`);
+});
+
+Deno.test("Basic table with padding", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file, padding: 2 });
+
+  await t.write([
+    ["hello world", "test"],
+    ["h", "t"],
+  ]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+        tableText,
+`\
+  hello world test  
+  h           t     \
+`);
+});
+
+Deno.test("None string values", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file });
+
+  await t.write([[123, 456]]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+      tableText,
+`\
+123 456\
+`);
+});
+
+Deno.test("null value in cell", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file });
+
+  await t.write([
+    ["a", "b"],
+    [null, "c"],
+  ]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+      tableText,
+`\
+a b
+  c\
+`);
+});
+
+Deno.test("undefined value in cell", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file });
+
+  await t.write([
+    ["a", "b"],
+    [undefined, "c"],
+  ]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+      tableText,
+`\
+a b
+  c\
+`);
+});
+
+Deno.test("missing value in cell", async () => {
+  const f = await createFile();
+  const t = new Table({ output: f.file });
+
+  await t.write([["a", "b"], ["c"], ["d", "e"]]);
+
+  const tableText = await readFileContent(f);
+
+  // prettier-ignore
+  assertEquals(
+      tableText,
+`\
+a b
+c  
+d e\
+`);
+});
---
+++ pretty-table.ts
@@ -0,0 +1,101 @@
+export default class PrettyTable {
+  private output: Deno.Writer & Deno.WriterSync & Deno.Closer;
+  private padding: number;
+  private spacing: number;
+  constructor(conf: PrettyTableConfig) {
+    this.output = conf?.output ?? Deno.stdout;
+    this.padding = conf?.padding ?? 0;
+    this.spacing = conf?.spacing ?? 1;
+  }
+
+  private async writeCol(
+    content: string,
+    contentWidth?: number,
+    padRight?: number,
+    padLeft?: number
+  ): Promise<void> {
+    content = content?.trim() ?? "";
+
+    if (contentWidth) {
+      content = content.padEnd(contentWidth);
+    }
+
+    if (padRight) {
+      content = `${content}${" ".repeat(padRight)}`;
+    }
+
+    if (padLeft) {
+      content = `${" ".repeat(padLeft)}${content}`;
+    }
+
+    await this.writeText(content);
+  }
+
+  async writeText(text: string): Promise<void> {
+    await this.output.write(new TextEncoder().encode(text));
+  }
+
+  async write(table: any[][]): Promise<void> {
+    const widths: number[] = [];
+
+    for (let row of table) {
+      for (let [index, col] of row.entries()) {
+        row[index] = (col ?? "") + "";
+        const colLength = row[index]?.length ?? 0;
+        if (!widths[index] || widths[index] < colLength) {
+          widths[index] = colLength;
+        }
+      }
+    }
+
+    for (let [i, row] of table.entries()) {
+      if (i !== 0) {
+        await this.writeText("\n");
+      }
+      await this.writeText(`${" ".repeat(this.padding)}`);
+
+      for (let [j, col] of row.entries()) {
+        if (typeof col !== "string") {
+          throw new Error("no string");
+        }
+
+        if (j === 0) {
+          await this.writeCol(col, widths[j]);
+        } else {
+          await this.writeCol(col, widths[j], 0, this.spacing);
+        }
+      }
+
+      await this.writeText(`${" ".repeat(this.padding)}`);
+    }
+  }
+}
+
+// https://www.compart.com/en/unicode/block/U+2500
+// ┏━┳┓
+// ┃━━┃
+// ┣━╋┫
+// ┗━┻┛
+
+const enum Border {
+  Horizontal = "━",
+  HorizontalDown = "┳",
+  HorizontalUp = "┻",
+  Vertical = "┃",
+  VerticalRight = "┣",
+  VerticalLeft = "┫",
+  TopLeft = "┏",
+  TopRight = "┓",
+  BottomLeft = "┗",
+  BottomRight = "┛",
+}
+export const enum Test {
+  test,
+  test1,
+}
+
+export interface PrettyTableConfig {
+  output?: Deno.Writer & Deno.WriterSync & Deno.Closer;
+  spacing?: number;
+  padding?: number;
+}
